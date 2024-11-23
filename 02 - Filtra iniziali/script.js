456b8be020d7d180959ec521f28ae7ccc10aaddd
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2024-11-23 02:55:58 +0100

    [add] first commit

---
+++ 02 - Filtra iniziali/script.js
@@ -0,0 +1,21 @@
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+
+const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
+
+
+// Dichiara la funzione qui.
+const inizianoPerA = (names) => {
+  let inizialiArray = []
+  for (let i = 0; i < names.length; i++)
+    if (names[i][0] == "A") {
+      inizialiArray.push(names[i])
+    }
+  return inizialiArray
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(inizianoPerA(names));
+
+
+//Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
---
+++ 03 - conta vocali/script.js
@@ -0,0 +1,24 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+
+const word = 'javascript';
+
+
+// Dichiara la funzione qui.
+function numbersOfVocals(string) {
+  const vocali = ['a', 'e', 'i', 'o', 'u']
+  const stringArray = string.split("");
+  let counter = 0;
+  for (let i = 0; i < stringArray.length; i++) {
+    if (vocali.includes(stringArray[i]))
+      counter++
+  }
+  return counter
+}
+
+
+// Invoca la funzione qui e stampa il risultato in console
+console.log(numbersOfVocals(word));
+
+
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
